673407d3857203c66ac3786700a87593070810e5
Krishnan Parthasarathi <kp@minio.io>
2021-03-03 10:15:34 -0800

    Add support for remote tier management
    With this change MinIO's ILM supports transitioning objects to a remote tier.
    This change includes support for Azure Blob Storage, AWS S3 compatible object
    storage incl. MinIO and Google Cloud Storage as remote tier storage backends.
    
    Some new additions include,
     - Admin APIs remote tier configuration management
    
     - Simple journal to track remote objects to be 'collected'
       This is used by object API handlers which 'mutate' object versions by
       overwriting/replacing content (Put/CopyObject) or removing the version
       itself (e.g DeleteObjectVersion).
    
     - Rework of previous ILM transition to fit the new model
       In the new model, a storage-class (a.k.a remote tier) is defined by the
       'remote' object storage type (one of s3, azure, gcs), bucket name and a
       prefix.
    
    Co-authored-by: Poorna Krishnamoorthy <poorna@minio.io>
    Co-authored-by: Krishna Srinivas <krishna@minio.io>

--- cmd/server-main.go
+++ cmd/server-main.go
@@ -235,6 +235,9 @@ func newAllSubsystems() {
 
 	// Create new bucket replication subsytem
 	globalBucketTargetSys = NewBucketTargetSys()
+
+	// Create new ILM tier configuration subsystem
+	globalTierConfigMgr = NewTierConfigMgr()
 }
 
 func initServer(ctx context.Context, newObject ObjectLayer) error {
@@ -397,6 +400,13 @@ func initAllSubsystems(ctx context.Context, newObject ObjectLayer) (err error) {
 	// Initialize bucket targets sub-system.
 	globalBucketTargetSys.Init(ctx, buckets, newObject)
 
+	if globalIsErasure {
+		// Initialize transition tier configuration manager
+		err = globalTierConfigMgr.Init(ctx, newObject)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -501,7 +511,6 @@ func serverMain(ctx *cli.Context) {
 	if err != nil {
 		logFatalErrs(err, Endpoint{}, true)
 	}
-
 	logger.SetDeploymentID(globalDeploymentID)
 
 	// Enable background operations for erasure coding
@@ -529,7 +538,12 @@ func serverMain(ctx *cli.Context) {
 
 	if globalIsErasure { // to be done after config init
 		initBackgroundReplication(GlobalContext, newObject)
+		globalTierJournal, err = initTierDeletionJournal(GlobalContext.Done())
+		if err != nil {
+			logger.FatalIf(err, "Unable to initialize remote tier pending deletes journal")
+		}
 	}
+
 	if globalCacheConfig.Enabled {
 		// initialize the new disk cache objects.
 		var cacheAPI CacheObjectLayer
